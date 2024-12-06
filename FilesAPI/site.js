fa74456d86ff4f3faeb074284f9404289e930afc
julian <julian.bliemel@gmx.de>
2024-12-06 19:52:06 +0100

    Fertig

--- FilesAPI/site.js
+++ FilesAPI/site.js
@@ -18,23 +18,87 @@
         event.preventDefault();
 
         if (event.originalEvent.dataTransfer.items) {
-            // Use DataTransferItemList interface to access the file(s)
-            [...event.originalEvent.dataTransfer.items].forEach((item, i) => {
-                // If dropped items aren't files, reject them
+
+                [...event.originalEvent.dataTransfer.items].forEach((item, i) => {
+
+
                 if (item.kind === "file") {
+
                     const file = item.getAsFile();
+
+
                     if (file.name.endsWith(".txt")) {
+
                         console.log(`… file[${i}].name = ${file.name}`);
+
+
+                        file.text().then(data => {
+
+                            data = data.toLowerCase();
+
+
+                            let result = [...data.matchAll(/[a-z]/g)].reduce((acc, char) => {
+                                acc[char[0]] = (acc[char[0]] || 0) + 1;
+                                return acc;
+                            }, {});
+
+
+                            let resultSorted = Object.entries(result).sort((a, b) => b[1] - a[1]);
+
+                            $("#result_div").prepend(`<h5>Character Count<br />( ${file.name} )</h5>`);
+
+                            for (const [char, count] of resultSorted) {
+                                console.log(`${char}: ${count}`);
+                                $("#resultlist").append(`<li>${char} : ${count}</li>`);
+                            }
+
+                        });
+
+
                     }
                     else {
+
                         console.log(`Not a Text File`);
+
+                        alert("You can only upload *.txt files");
                     }
+
+
                 }
+
+
             });
-        } else {
-            // Use DataTransfer interface to access the file(s)
+        }
+        else {
+
             [...event.originalEvent.dataTransfer.files].forEach((file, i) => {
                 console.log(`… file[${i}].name = ${file.name}`);
+
+                if (file[i].name.endsWith(".txt")) {
+                    file.text().then(data => {
+
+                        data = data.toLowerCase();
+
+                        let result = [...data.matchAll(/[a-z]/g)].reduce((acc, char) => {
+                            acc[char[0]] = (acc[char[0]] || 0) + 1;
+                            return acc;
+                        }, {});
+
+                        let resultSorted = Object.fromEntries(Object.entries(result).sort((a, b) => b[1] - a[1]));
+
+                        $("#result_div").prepend(`<h5>Character Count<br />( ${file.name} )</h5>`);
+
+                        for (const [char, count] of resultSorted) {
+                            console.log(`${char}: ${count}`);
+                            $("#resultlist").append(`<li>${char} : ${count}</li>`);
+                        }
+
+                    });
+                }
+                else {
+                    alert("You can only upload *.txt files");
+                }
+
             });
         }
     });
